fc89a5e6d61474a6948bd41f7a256f2f2095979a
Matthew <84196639+mteam88@users.noreply.github.com>
2026-03-26 18:14:31 -0400

    update stdev values for vision for kalman filter

--- src/main/java/frc/robot/RobotState.java
+++ src/main/java/frc/robot/RobotState.java
@@ -62,7 +62,7 @@ public final class RobotState {
                 drive.getModulePositionsSnapshot(),
                 latestDeadReckonedPose,
                 VecBuilder.fill(0.1, 0.1, 0.1),
-                VecBuilder.fill(1.3, 1.3, 1.3));
+                VecBuilder.fill(2.0, 2.0, 1.6));
         logState();
     }
 
