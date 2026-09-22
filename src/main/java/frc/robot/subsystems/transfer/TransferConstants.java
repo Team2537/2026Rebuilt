225339e9a858e91f0e5ee10de6a3a474b351adb5
Carina <carina.huang05@gmail.com>
2026-09-22 18:34:51 -0400

    Make basic transfer subsystem

---
+++ src/main/java/frc/robot/subsystems/transfer/TransferConstants.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.transfer;
+
+public final class TransferConstants {
+    public static final int TRANSFER_MOTOR_ID=14;
+
+    public static final double STATUS_UPDATE_HZ=50.0;
+
+}
